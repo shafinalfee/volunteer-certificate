c00c7e68bab72b7a653e548871936d528a19e079
shafinalfee <shafin.alfee@gmail.com>
2021-04-15 22:35:45 +0600

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const nameInput = document.getElementById('name')
+const insInput = document.getElementById('ins')
+const downloadBtn = document.getElementById('download-btn')
+
+ 
+
+
+const image = new Image()
+image.src = 'volunteer-01.jpg'
+image.onload = function(){
+      drawImage()
+
+}
+
+function drawImage(){
+    ctx.drawImage(image, 0, 2,canvas.width,canvas.height)
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(655, 260);
+    ctx.lineTo(655, 260);
+    ctx.stroke();
+    
+    ctx.font = "30px Adorable regular"
+    ctx.fillStyle='#FFFFFF'
+    ctx.textAlign = "center"; 
+    ctx.fillText(nameInput.value, 655, 260)
+    ctx.moveTo(410, 330);
+    ctx.lineTo(410, 330);
+    ctx.stroke();
+    ctx.font = "22px Adorable regular"
+    ctx.fillText(insInput.value, 410, 325)
+    
+}
+nameInput.addEventListener('input',function(){
+    drawImage()
+})
+insInput.addEventListener('input',function(){
+    drawImage()
+})
+ 
+
+
+downloadBtn.addEventListener('click', function(){
+   downloadBtn.href = canvas.toDataURL()
+   downloadBtn.download = true
+})
